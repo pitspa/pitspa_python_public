af52c6901547f76906a9efa6605e358cc1aa3ea4
pitspa <65638417+pitspa@users.noreply.github.com>
2020-06-13 14:32:49 +0200

    Update Heston_closed_form.py

--- Heston_closed_form.py
+++ Heston_closed_form.py
@@ -7,14 +7,20 @@ import matplotlib.pyplot as mp
 # thanks to https://mfe.baruch.cuny.edu/wp-content/uploads/2015/06/VW2.pdf
 # for the characteristic function form written in R
 
-T = 1
-eta = 0.09 # vol of vol
-lam = 0.5 # mean rev
-rho = -0.8 # corr
-v_ = 0.04 # long term vol
-v0 = 0.01 # initial vol
-
 def phi(u,T,eta,lam,rho,v_,v0):
+    
+    """
+    returns the exact Heston characteristic function in a range
+    of frequencies.
+    parameters:
+        u: the frequency range
+        T: maturity
+        eta: vol of vol
+        lam: mean reversion speed
+        rho: correlation
+        v_: long term volatility
+        v0: initial volatility condition 
+    """
     
     a = -u**2/2 - 1j*u/2
     b = lam - 1j*rho*eta*u
@@ -29,6 +35,13 @@ def phi(u,T,eta,lam,rho,v_,v0):
     
     return numpy.exp(C*v_+D*v0)
 
+T = 1
+eta = 0.09 
+lam = 0.5 
+rho = -0.8 
+v_ = 0.04 
+v0 = 0.01 
+
 N = 2000
 u = numpy.linspace(-200,200,N)
 du = numpy.diff(u)[0]
@@ -36,6 +49,20 @@ du = numpy.diff(u)[0]
 f = phi(u,T,eta,lam,rho,v_,v0)  
 
 def pdf(x,u,T,eta,lam,rho,v_,v0):
+    
+    """
+    approximation of the inverse continuous Fourier transform of the
+    Heston characteristic function.
+    parameters:
+        x: the inverse-frequency range
+        u: the frequency range
+        T: maturity
+        eta: vol of vol
+        lam: mean reversion speed
+        rho: correlation
+        v_: long term volatility
+        v0: initial volatility condition 
+    """
     
     cf = phi(u,T,eta,lam,rho,v_,v0)
     
@@ -63,13 +90,6 @@ ax2.set_xlim((-1,1))
 mp.show()
 
 print(numpy.sum(P)*dx)
-
-mp.plot(u,numpy.real(f),'b')
-mp.plot(u,numpy.imag(f),'r')
-mp.xlim((-50,50))
-mp.ylabel('CF(h)')
-mp.xlabel('h')
-mp.show()
 
 
 
